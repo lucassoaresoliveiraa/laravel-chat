9f7b614fbf05c678bdb0313de30269b42f22a57a
Lucas Soares <lucas.ssoares.oliveiira@gmail.com>
2023-05-10 13:10:21 -0300

    Initial commit

---
+++ app/Http/Controllers/SocketsController.php
@@ -0,0 +1,30 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Broadcasting\Broadcasters\PusherBroadcaster;
+use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Return_;
+use Pusher\Pusher;
+
+class SocketsController
+{
+    public function connect(Request $request)
+    {
+        $broadcaster = new PusherBroadcaster(
+            new Pusher(
+                env("PUSHER_APP_KEY"),
+                env("PUSHER_APP_SECRET"),
+                env("PUSHER_APP_ID"),
+                []
+
+            )
+        );
+        return $broadcaster->validAuthenticationResponse($request, []);
+    }
+}
+
+
+?>
---
+++ routes/channels.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Broadcast;
+
+
+
+Broadcast::channel("SendMessageEvent", function(){
+    return[
+        "name" => $this->name,
+        "message" => $this->message,
+        "time" => $this->time
+    ];
+});
